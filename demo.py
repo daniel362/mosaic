6f9771502ff580b28bc974fc60150caff6ff97d0
Daim Tak <histuckyi@gmail.com>
2018-11-22 19:59:58 +0900

    Add  new file : demo, mosic.py

---
+++ demo.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+import cv2
+
+
+def demo():
+    pass
+
+if __name__ == "__main__":
+    print('demo')
---
+++ mosaic.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+def mosaic():
+    pass
+
+if __name__ == "__main__":
+    print('mosaic')
