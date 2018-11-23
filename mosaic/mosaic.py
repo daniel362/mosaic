a3113a14ac63af9414566ccb032cd42193bf0135
Daim Tak <histuckyi@gmail.com>
2018-11-23 18:37:26 +0900

    Modify  change location mosic.py from . to /mosaic

---
+++ mosaic/mosaic.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+class Mosaic(object):
+
+    def __init__(self, origin=False, *args):
+        pass
+
+    def blending(self):
+        pass
+
+    def transform(self):
+        pass
+
+    def translation(self):
+        pass
+
+    def merging(self):
+        pass
+
+
+if __name__ == "__main__":
+    print('mosaic')
